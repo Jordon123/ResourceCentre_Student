3e8026bae859e8f90c5d64e554a41a677356decf
21000300 <21000300@10.174.115.215>
2022-07-15 12:09:41 +0800

    Jordon Testing 123

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -60,7 +60,21 @@ public class ResourceCentreTest {
 	public void testAddChromebook() {
 		//fail("Not yet implemented");
 		// write your code here 
-		
+		// Item list is not null, so that can add a new item
+				assertNotNull("Test if there is valid Chromebook arraylist to add to", chromebookList);
+				
+				//Given an empty list, after adding 1 item, the size of the list is 1
+				ResourceCentre.addChromebook(chromebookList, cb1);		
+				assertEquals("Test if that chromebook arraylist size is 1?", 1, camcorderList.size());
+				
+				//The item just added is as same as the first item of the list
+				assertSame("Test that chromebook is added same as 1st item of the list?", cc1, camcorderList.get(0));
+				
+				//Add another item. test The size of the list is 2?
+				ResourceCentre.addCamcorder(camcorderList, cc2);
+				ResourceCentre.addCamcorder(camcorderList, cc3);
+				assertEquals("Test that Camcorder arraylist size is 3?", 3, camcorderList.size());
+				assertSame("Test that Camcorder is added same as 3rd item of the list?", cc3, camcorderList.get(2));
 	}
 	
 	@Test
