1f3f43ffcd73bf800e013cb1d69eb4087f11fcec
21015516 <21015516@10.174.115.101>
2022-07-15 12:52:21 +0800

    pushing please

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -12,6 +12,9 @@ public class ResourceCentre {
 		camcorderList.add(new Camcorder("CC002", "Panasonic HC-MDH2", 10));
 		chromebookList.add(new Chromebook("CB001", "ASUS Chromebook ", "Win 10"));
 		chromebookList.add(new Chromebook("CB002", "HP Chromebook", "Win 10"));
+		
+		// pushing 
+		
 <<<<<<< HEAD
 <<<<<<< HEAD
  
